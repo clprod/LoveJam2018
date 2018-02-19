b9e249235485c3920158bfa5661017c8109344df
DoubleL73 <leolafiosca@gmail.com>
2018-02-19 17:15:17 +0100

    added some waves

---
+++ waves/wave5.lua
@@ -0,0 +1,10 @@
+return {
+  spawn = {
+    {time = 1, type = 0, number = 3, pos = Vector(130, 30)},
+    {time = 2, type = 0, number = 5, pos = Vector(400, 30)},
+    {time = 3, type = 0, number = 3, pos = Vector(700, 30)},
+    {time = 6, type = 0, number = 3, pos = Vector(30, 550)},
+    {time = 7, type = 0, number = 5, pos = Vector(425, 30)},
+    {time = 8, type = 0, number = 3, pos = Vector(425, 550)},
+  }
+}
